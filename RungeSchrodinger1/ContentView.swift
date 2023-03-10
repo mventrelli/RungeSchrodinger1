24b0b22d0ad389128693fa27efd74e6fe7525d4f
Matt Ventrelli <mventrelli@hawk.iit.edu>
2023-03-10 14:30:22 -0600

    Initial Commit

---
+++ RungeSchrodinger1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RungeSchrodinger1
+//
+//  Created by IIT PHYS 440 on 3/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RungeSchrodinger1/RungeSchrodinger1App.swift
@@ -0,0 +1,17 @@
+//
+//  RungeSchrodinger1App.swift
+//  RungeSchrodinger1
+//
+//  Created by IIT PHYS 440 on 3/10/23.
+//
+
+import SwiftUI
+
+@main
+struct RungeSchrodinger1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
